aa268582c276c548cc9d22135ba0412aedbaf124
Kacper Różycki <138244778+UnivG@users.noreply.github.com>
2025-01-20 10:31:46 +0100

    Update page.js

--- test/app/page.js
+++ test/app/page.js
@@ -2,18 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <div>
       Test
       raewae
       AHa
       <h1>MHM</h1>
-=======
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-      <p>Kacper Zawiszewski</p>
-      
->>>>>>> 526703c568dade4f38658cc1ee87b0f3f4a21b9f
+
     </div>
   );
 }
